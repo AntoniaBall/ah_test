727febb9b5ccb4a641829548b7c13acf404b584f
AntoniaBall <antonia.balluais@gmail.com>
2020-12-16 23:47:22 +0100

    upload repository classes

--- ah_api/src/Repository/TypeValueRepository.php
+++ ah_api/src/Repository/TypeValueRepository.php
@@ -2,25 +2,25 @@
 
 namespace App\Repository;
 
-use App\Entity\Test;
+use App\Entity\TypeValue;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
- * @method Test|null find($id, $lockMode = null, $lockVersion = null)
- * @method Test|null findOneBy(array $criteria, array $orderBy = null)
- * @method Test[]    findAll()
- * @method Test[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ * @method TypeValue|null find($id, $lockMode = null, $lockVersion = null)
+ * @method TypeValue|null findOneBy(array $criteria, array $orderBy = null)
+ * @method TypeValue[]    findAll()
+ * @method TypeValue[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
-class TestRepository extends ServiceEntityRepository
+class TypeValueRepository extends ServiceEntityRepository
 {
     public function __construct(ManagerRegistry $registry)
     {
-        parent::__construct($registry, Test::class);
+        parent::__construct($registry, TypeValue::class);
     }
 
     // /**
-    //  * @return Test[] Returns an array of Test objects
+    //  * @return TypeValue[] Returns an array of TypeValue objects
     //  */
     /*
     public function findByExampleField($value)
@@ -37,7 +37,7 @@ class TestRepository extends ServiceEntityRepository
     */
 
     /*
-    public function findOneBySomeField($value): ?Test
+    public function findOneBySomeField($value): ?TypeValue
     {
         return $this->createQueryBuilder('t')
             ->andWhere('t.exampleField = :val')
