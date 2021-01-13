d61f11ede5f86af465d98c2e297c9ce2c30114d0
AntoniaBall <antonia.balluais@gmail.com>
2021-01-13 16:16:28 +0100

    add fixtures adress relation property

--- ah_api/src/DataFixtures/AddressFixture.php
+++ ah_api/src/DataFixtures/AddressFixture.php
@@ -6,24 +6,123 @@ use App\Entity\Address;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Faker;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 
-
-class AddressFixture extends Fixture
+class AddressFixture extends Fixture 
 {
+    public const adresse_bien = 'adresse_bien';
+    public const adr_cabane_eau = 'adr_cabane_eau';
+    public const adr_bulle= 'adr_bulle';
+    public const adr_roulotte= 'adr_roulotte';
+    public const adr_chalets= 'adr_chalets';
+    public const adr_inclassable= 'adr_inclassable';
+    public const adr_bateau= 'adr_bateau';
+    public const adr_tipi= 'adr_tipi';
+    public const adr_yourte= 'adr_yourte';
+
     public function load(ObjectManager $manager)
     {
         $generator = Faker\Factory::create();
-        for ($i = 0; $i < 10; $i++) {
-            $address = new Address();
-            $address->setNumber((int) $generator->numberBetween($min = 1, $max = 900))
+        $address= new Address();
+        $address->setNumber((int) $generator->numberBetween($min = 1, $max = 900))
                 ->setStreet($generator->streetName)
                 ->setPostalCode((int)$generator->postcode)
                 ->setTown($generator->city)
                 ->setRegion($generator->state)
                 ->setCountry($generator->country);
-            $manager->persist($address);
-        }
+        $manager->persist($address);
+
+        $this->addReference(self::adresse_bien,$address);
+
+        $addressEau= new Address();
+        $addressEau->setNumber((int) $generator->numberBetween($min = 1, $max = 900))
+                ->setStreet($generator->streetName)
+                ->setPostalCode((int)$generator->postcode)
+                ->setTown($generator->city)
+                ->setRegion($generator->state)
+                ->setCountry($generator->country);
+        $manager->persist($addressEau);
+
+        $this->addReference(self::adr_cabane_eau,$addressEau);
+        
+        $adr_tipi= new Address();
+        $adr_tipi->setNumber((int) $generator->numberBetween($min = 1, $max = 900))
+        ->setStreet($generator->streetName)
+        ->setPostalCode((int)$generator->postcode)
+        ->setTown($generator->city)
+        ->setRegion($generator->state)
+        ->setCountry($generator->country);
+        $manager->persist($adr_tipi);
+
+        $this->addReference(self::adr_tipi,$adr_tipi);
+        
+        $adr_bulle= new Address();
+        $adr_bulle->setNumber((int) $generator->numberBetween($min = 1, $max = 900))
+        ->setStreet($generator->streetName)
+        ->setPostalCode((int)$generator->postcode)
+        ->setTown($generator->city)
+        ->setRegion($generator->state)
+        ->setCountry($generator->country);
+        $manager->persist($adr_bulle);
+
+        $this->addReference(self::adr_bulle,$adr_bulle);
+        
+        $adr_bateau= new Address();
+        $adr_bateau->setNumber((int) $generator->numberBetween($min = 1, $max = 900))
+                ->setStreet($generator->streetName)
+                ->setPostalCode((int)$generator->postcode)
+                ->setTown($generator->city)
+                ->setRegion($generator->state)
+                ->setCountry($generator->country);
+        $manager->persist($adr_bateau);
+
+        $this->addReference(self::adr_bateau,$adr_bateau);
+
+        $adr_roulotte= new Address();
+        $adr_roulotte->setNumber((int) $generator->numberBetween($min = 1, $max = 900))
+                ->setStreet($generator->streetName)
+                ->setPostalCode((int)$generator->postcode)
+                ->setTown($generator->city)
+                ->setRegion($generator->state)
+                ->setCountry($generator->country);
+        $manager->persist($adr_roulotte);
+
+        $this->addReference(self::adr_roulotte,$adr_roulotte);
+
+        $adr_chalets= new Address();
+        $adr_chalets->setNumber((int) $generator->numberBetween($min = 1, $max = 900))
+                ->setStreet($generator->streetName)
+                ->setPostalCode((int)$generator->postcode)
+                ->setTown($generator->city)
+                ->setRegion($generator->state)
+                ->setCountry($generator->country);
+        $manager->persist($adr_chalets);
+
+        $this->addReference(self::adr_chalets,$adr_chalets);
+        
+        $adr_inclassable= new Address();
+        $adr_inclassable->setNumber((int) $generator->numberBetween($min = 1, $max = 900))
+            ->setStreet($generator->streetName)
+            ->setPostalCode((int)$generator->postcode)
+            ->setTown($generator->city)
+            ->setRegion($generator->state)
+            ->setCountry($generator->country);
+        $manager->persist($adr_inclassable);
+        
+        $this->addReference(self::adr_inclassable,$adr_inclassable);
+
+        $adr_yourte= new Address();
+        $adr_yourte->setNumber((int) $generator->numberBetween($min = 1, $max = 900))
+                ->setStreet($generator->streetName)
+                ->setPostalCode((int)$generator->postcode)
+                ->setTown($generator->city)
+                ->setRegion($generator->state)
+                ->setCountry($generator->country);
+        $manager->persist($adr_yourte);
+
+        $this->addReference(self::adr_yourte,$adr_yourte);
 
         $manager->flush();
     }
+
 }
--- ah_api/src/DataFixtures/PropertyFixtures.php
+++ ah_api/src/DataFixtures/PropertyFixtures.php
@@ -34,10 +34,11 @@ class PropertyFixtures extends Fixture implements DependentFixtureInterface
             ->setNbrRoom(3)
             ->setMaxTravelers(3)
             ->setTax(0.20)
+            ->setAddress($this->getReference(AddressFixture::adresse_bien))
             ->addPicture($this->getReference(PicturesFixtures::picture_bien));
             
             $this->addReference(self::CABANES,$cabane);
-
+            
             $manager->persist($cabane);
             
             $cabaneEau = new Property();
@@ -51,6 +52,7 @@ class PropertyFixtures extends Fixture implements DependentFixtureInterface
             ->setNbrRoom(3)
             ->setMaxTravelers(3)
             ->setTax(0.20)
+            ->setAddress($this->getReference(AddressFixture::adr_cabane_eau))
             ->addPicture($this->getReference(PicturesFixtures::picture_bien));
             
             $this->addReference(self::CABANES_EAU,$cabaneEau);
@@ -67,6 +69,7 @@ class PropertyFixtures extends Fixture implements DependentFixtureInterface
             ->setNbrRoom(3)
             ->setMaxTravelers(3)
             ->setTax(0.20)
+            ->setAddress($this->getReference(AddressFixture::adr_bulle))
             ->addPicture($this->getReference(PicturesFixtures::picture_bien));
             
             $this->addReference(self::bulles,$bulle);
@@ -83,21 +86,24 @@ class PropertyFixtures extends Fixture implements DependentFixtureInterface
             ->setNbrRoom(3)
             ->setMaxTravelers(3)
             ->setTax(0.20)
+            ->setAddress($this->getReference(AddressFixture::adr_tipi))
             ->addPicture($this->getReference(PicturesFixtures::picture_bien));
             $this->addReference(self::tipis,$tipi);
             $manager->persist($tipi);
             
             $roulottes = new Property();
             $roulottes->setTitle('roulottes')
-                ->setDescription('très belle cabane sur l\'eau. Sit amet jelly beans pie apple pie chupa chups candy. I love candy I love pie bear claw chocolate bar sweet tootsie roll I love..')
-                ->setAccessHandicap(false)
-                ->setWater('eau courante')
-                ->setElectricity(true)
-                ->setSurface(30)
-                ->setRate(95.50)
-                ->setNbrRoom(3)
-                ->setMaxTravelers(3)
-                ->setTax(0.20);
+            ->setDescription('très belle cabane sur l\'eau. Sit amet jelly beans pie apple pie chupa chups candy. I love candy I love pie bear claw chocolate bar sweet tootsie roll I love..')
+            ->setAccessHandicap(false)
+            ->setWater('eau courante')
+            ->setElectricity(true)
+            ->setSurface(30)
+            ->setRate(95.50)
+            ->setNbrRoom(3)
+            ->setMaxTravelers(3)
+            ->setAddress($this->getReference(AddressFixture::adr_roulotte))
+            ->addPicture($this->getReference(PicturesFixtures::picture_bien))
+            ->setTax(0.20);
             $this->addReference(self::roulottes,$roulottes);
             $manager->persist($roulottes);
             
@@ -112,230 +118,63 @@ class PropertyFixtures extends Fixture implements DependentFixtureInterface
             ->setNbrRoom(3)
             ->setMaxTravelers(3)
             ->setTax(0.20)
-            ->addPicture($this->getReference(PicturesFixtures::picture_bien));
-
-        $this->addReference(self::chalets,$chalets);
-        $manager->persist($chalets);
-
-        $yourtes = new Property();
-        $yourtes->setTitle('yourtes')
-            ->setDescription('très belle cabane sur l\'eau. Sit amet jelly beans pie apple pie chupa chups candy. I love candy I love pie bear claw chocolate bar sweet tootsie roll I love..')
-            ->setAccessHandicap(false)
-            ->setWater('eau courante')
-            ->setElectricity(true)
-            ->setSurface(30)
-            ->setRate(95.50)
-            ->setNbrRoom(3)
-            ->setMaxTravelers(3)
-            ->setTax(0.20)
-            ->addPicture($this->getReference(PicturesFixtures::picture_bien));
-        $this->addReference(self::yourtes,$yourtes);
-        $manager->persist($yourtes);
-
-        $inclassables = new Property();
-        $inclassables->setTitle('inclassables')
-            ->setDescription('très belle cabane sur l\'eau. Sit amet jelly beans pie apple pie chupa chups candy. I love candy I love pie bear claw chocolate bar sweet tootsie roll I love..')
-            ->setAccessHandicap(false)
-            ->setWater('eau courante')
-            ->setElectricity(true)
-            ->setSurface(30)
-            ->setRate(95.50)
-            ->setNbrRoom(3)
-            ->setMaxTravelers(3)
-            ->setTax(0.20)
-            ->addPicture($this->getReference(PicturesFixtures::picture_bien));
-        $this->addReference(self::inclassables,$inclassables);
-        $manager->persist($inclassables);
-
-        $bateaux = new Property();
-        $bateaux->setTitle('bateaux')
-            ->setDescription('très belle cabane sur l\'eau. Sit amet jelly beans pie apple pie chupa chups candy. I love candy I love pie bear claw chocolate bar sweet tootsie roll I love..')
-            ->setAccessHandicap(false)
-            ->setWater('eau courante')
-            ->setElectricity(true)
-            ->setSurface(30)
-            ->setRate(95.50)
-            ->setNbrRoom(3)
-            ->setMaxTravelers(3)
-            ->setTax(0.20)
+            ->setAddress($this->getReference(AddressFixture::adr_chalets))
             ->addPicture($this->getReference(PicturesFixtures::picture_bien));
             
-        $this->addReference(self::bateaux,$bateaux);
-        $manager->persist($bateaux);
-        $manager->flush();
-
-        // // // 15 biens dans la catégorie cabane dans les arbres
-        // for ($i = 0; $i <= 100; $i++) {
-        //     $property = new Property();
-        //     $property->setTitle('cabane dans les abres');
-        //     $property->setDescription('Description n° '.$i.' très beau chalet à la montagne. Sit amet jelly beans pie apple pie chupa chups candy. I love candy I love pie bear claw chocolate bar sweet tootsie roll I love..');
-        //     $property->setAccessHandicap(true);
-        //     $property;
-        //     $property->setElectricity(true);
-        //     $property->setSurface(150);
-        //     $property->setSurface(150);
-        //     $property->setRate(95.50);
-        //     $property->setMaxTravelers(3);
-        //     $property->setTax(10.20);
-        //     // $repository = $this->getDoctrine()->getRepository(typeProperty::class);
-        //     // $typeProperty = $repository->find(1);
-        //     // $property->setTypeProperty($typeProperty);  // 1 cabane dans les arbres
-        //     $manager->persist($property);
+            $this->addReference(self::chalets,$chalets);
+            $manager->persist($chalets);
             
-        // }
+            $yourtes = new Property();
+            $yourtes->setTitle('yourtes')
+            ->setDescription('très belle cabane sur l\'eau. Sit amet jelly beans pie apple pie chupa chups candy. I love candy I love pie bear claw chocolate bar sweet tootsie roll I love..')
+            ->setAccessHandicap(false)
+            ->setWater('eau courante')
+            ->setElectricity(true)
+            ->setSurface(30)
+            ->setRate(95.50)
+            ->setNbrRoom(3)
+            ->setMaxTravelers(3)
+            ->setTax(0.20)
+            ->setAddress($this->getReference(AddressFixture::adr_yourte))
+            ->addPicture($this->getReference(PicturesFixtures::picture_bien));
+            $this->addReference(self::yourtes,$yourtes);
+            $manager->persist($yourtes);
+            
+            $inclassables = new Property();
+            $inclassables->setTitle('inclassables')
+            ->setDescription('très belle cabane sur l\'eau. Sit amet jelly beans pie apple pie chupa chups candy. I love candy I love pie bear claw chocolate bar sweet tootsie roll I love..')
+            ->setAccessHandicap(false)
+            ->setWater('eau courante')
+            ->setElectricity(true)
+            ->setSurface(30)
+            ->setRate(95.50)
+            ->setNbrRoom(3)
+            ->setMaxTravelers(3)
+            ->setTax(0.20)
+            ->setAddress($this->getReference(AddressFixture::adr_inclassable))
+            ->addPicture($this->getReference(PicturesFixtures::picture_bien));
+            $this->addReference(self::inclassables,$inclassables);
+            $manager->persist($inclassables);
+            
+            $bateaux = new Property();
+            $bateaux->setTitle('bateaux')
+            ->setDescription('très belle cabane sur l\'eau. Sit amet jelly beans pie apple pie chupa chups candy. I love candy I love pie bear claw chocolate bar sweet tootsie roll I love..')
+            ->setAccessHandicap(false)
+            ->setWater('eau courante')
+            ->setElectricity(true)
+            ->setSurface(30)
+            ->setRate(95.50)
+            ->setNbrRoom(3)
+            ->setMaxTravelers(3)
+            ->setTax(0.20)
+            ->setAddress($this->getReference(AddressFixture::adr_bateau))
+            ->addPicture($this->getReference(PicturesFixtures::picture_bien));
+            
+            $this->addReference(self::bateaux,$bateaux);
+            $manager->persist($bateaux);
 
-        // // // 15 biens dans la catégorie cabane sur l'eau
-        // for ($i = 16; $i <= 25; $i++) {
-        //     $property = new Property();
-        //     $property->setTitle('catégorie cabane sur l\'eau n°');
-        //     $property->setDescription('Description n° '.$i.' très beau chalet à la montagne. Sit amet jelly beans pie apple pie chupa chups candy. I love candy I love pie bear claw chocolate bar sweet tootsie roll I love..');
-        //     $property->setAccessHandicap(false);
-        //     $property->setWater('eau du puits');
-        //     $property->setElectricity(false);
-        //     $property->setSurface(30);
-        //     $property->setNbrRoom(1);
-        //     $property->setRate(50);
-        //     $property->setMaxTravelers(2);
-        //     $property->setTax(0.20);
-        //     // $property->setTypeProperty();  // 2 cabane sur l'eau.
-        //     $manager->persist($property);
-        // }
-        // // // 15 biens dans la catégorie bulle
-        // for ($i = 26; $i <= 40; $i++) {
-        //     $property = new Property();
-        //     $property->setTitle('Bulle');
-        //     $property->setDescription('Description n° '.$i.' très beau chalet à la montagne. Sit amet jelly beans pie apple pie chupa chups candy. I love candy I love pie bear claw chocolate bar sweet tootsie roll I love..');
-        //     $property->setAccessHandicap(false);
-        //     $property->setWater('eau du puits');
-        //     $property->setElectricity(false);
-        //     $property->setSurface(30);
-        //     $property->setNbrRoom(1);
-        //     $property->setRate(50);
-        //     $property->setMaxTravelers(2);
-        //     $property->setTax(0.20);
-        //     // $property->setTypeProperty();  // 3 bulle
-        //     $manager->persist($property);
-        // }
-        // // // 15 biens dans la catégorie Tipi
-        // for ($i = 41; $i <= 55; $i++) {
-        //     $property = new Property();
-        //     $property->setTitle('Tipi');
-        //     $property->setDescription('Description n° '.$i.' très beau chalet à la montagne. Sit amet jelly beans pie apple pie chupa chups candy. I love candy I love pie bear claw chocolate bar sweet tootsie roll I love..');
-        //     $property->setAccessHandicap(false);
-        //     $property->setWater('eau du puits');
-        //     $property->setElectricity(false);
-        //     $property->setSurface(30);
-        //     $property->setNbrRoom(1);
-        //     $property->setRate(50);
-        //     $property->setMaxTravelers(2);
-        //     $property->setTax(0.20);
-        //     // $property->setTypeProperty();  // 4 tipi
-        //     $manager->persist($property); 
-        // }
-        // // // 10 biens dans la catégorie Yourte
-        // for ($i = 56; $i <= 65; $i++) {
-        //     $property = new Property();
-        //     $property->setTitle('Yourte');
-        //     $property->setDescription('Description n° '.$i.' très beau chalet à la montagne. Sit amet jelly beans pie apple pie chupa chups candy. I love candy I love pie bear claw chocolate bar sweet tootsie roll I love..');
-        //     $property->setAccessHandicap(false);
-        //     $property->setWater('eau du puits');
-        //     $property->setElectricity(false);
-        //     $property->setSurface(30);
-        //     $property->setNbrRoom(1);
-        //     $property->setRate(50);
-        //     $property->setMaxTravelers(2);
-        //     $property->setTax(0.20);
-        //     // $property->setTypeProperty();  // 5 Yourte
-        //     $manager->persist($property); 
-        // }
-        // // // 15 biens dans la catégorie Roulotte
-        // for ($i = 66; $i <= 80; $i++) {
-        //     $property = new Property();
-        //     $property->setTitle('Roulotte');
-        //     $property->setDescription('Description n° '.$i.' très beau chalet à la montagne. Sit amet jelly beans pie apple pie chupa chups candy. I love candy I love pie bear claw chocolate bar sweet tootsie roll I love..');
-        //     $property->setAccessHandicap(false);
-        //     $property->setWater('eau du puits');
-        //     $property->setElectricity(false);
-        //     $property->setSurface(30);
-        //     $property->setNbrRoom(1);
-        //     $property->setRate(50);
-        //     $property->setMaxTravelers(2);
-        //     $property->setTax(0.20);
-        //     // $property->setTypeProperty();  // 6 Roulotte
-        //     $manager->persist($property); 
-        // }
-        // // // 15 biens dans la catégorie Bateau
-        // for ($i = 81; $i <= 95; $i++) {
-        //     $property = new Property();
-        //     $property->setTitle('Bateau');
-        //     $property->setDescription('Description n° '.$i.' très beau chalet à la montagne. Sit amet jelly beans pie apple pie chupa chups candy. I love candy I love pie bear claw chocolate bar sweet tootsie roll I love..');
-        //     $property->setAccessHandicap(false);
-        //     $property->setWater('eau du puits');
-        //     $property->setElectricity(false);
-        //     $property->setSurface(30);
-        //     $property->setNbrRoom(1);
-        //     $property->setRate(50);
-        //     $property->setMaxTravelers(2);
-        //     $property->setTax(0.20);
-        //     // $property->setTypeProperty();  // 7 Bateau
-        //     $manager->persist($property); 
-        // }
-        // // // 15 biens dans la catégorie Chalet
-        // for ($i = 96; $i <= 111; $i++) {
-        //     $property = new Property();
-        //     $property->setTitle('Chalet');
-        //     $property->setDescription('Description n° '.$i.' très beau chalet à la montagne. Sit amet jelly beans pie apple pie chupa chups candy. I love candy I love pie bear claw chocolate bar sweet tootsie roll I love..');
-        //     $property->setAccessHandicap(false);
-        //     $property->setWater('eau du puits');
-        //     $property->setElectricity(false);
-        //     $property->setSurface(30);
-        //     $property->setNbrRoom(1);
-        //     $property->setRate(50);
-        //     $property->setMaxTravelers(2);
-        //     $property->setTax(0.20);
-        //     // $property->setTypeProperty();  // 8 Chalet
-        //     $manager->persist($property);
-        // }
-        
-        // // 13 biens dans la catégorie inclassable
-        // for ($i = 112; $i < 125; $i++) {
-        //     $property = new Property();
-        //     $property->setTitle('Igloo inclassable n°');
-        //     $property->setDescription('Description n° '.$i.' très beau chalet à la montagne. Sit amet jelly beans pie apple pie chupa chups candy. I love candy I love pie bear claw chocolate bar sweet tootsie roll I love..');
-        //     $property->setAccessHandicap(false);
-        //     $property->setWater('eau du puits');
-        //     $property->setElectricity(false);
-        //     $property->setSurface(30);
-        //     $property->setNbrRoom(1);
-        //     $property->setRate(50);
-        //     $property->setMaxTravelers(2);
-        //     $property->setTax(0.20);
-        //     // $property->setTypeProperty();  // 9 Inclassasble
-        //     $manager->persist($property);
-        // }
-
-        $manager->flush();
+            $manager->flush();
     }
-
-    // protected function loadData(ObjectManager $manager)
-    // {
-    //     $this->createMany(Property::class, 100, function(Property $property){
-    //         $property->setTitle('Igloo inclassable n°');
-    //         $property->setDescription('Description n° '.$i.' très beau chalet à la montagne. Sit amet jelly beans pie apple pie chupa chups candy. I love candy I love pie bear claw chocolate bar sweet tootsie roll I love..');
-    //         $property->setAccessHandicap(false);
-    //         $property->setWater('eau du puit');
-    //         $property->setElectricity(false);
-    //         $property->setSurface(30);
-    //         $property->setNbrRoom(1);
-    //         $property->setRate(50);
-    //         $property->setMaxTravelers(2);
-    //         $property->setTax(0.20);
-    //         $property->setTypeProperty($this->addReference(TypeProperty::class, '_0'));  // 9 Inclassable
-    //         $manager->persist($property);
-
-    //     });
-        // $manager->flush();
-    // }
 
     public function getDependencies()
     {
