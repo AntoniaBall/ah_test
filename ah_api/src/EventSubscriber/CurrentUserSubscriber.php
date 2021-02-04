214bc14fd7031f340103ae34f62d2d50d9e15f14
AntoniaBall <antonia.balluais@gmail.com>
2021-02-04 22:40:17 +0100

    set current user to properties created

---
+++ ah_api/src/EventSubscriber/CurrentUserSubscriber.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use ApiPlatform\Core\EventListener\EventPriorities;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\GetResponseForControllerResultEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+use Symfony\Component\HttpKernel\Event\ViewEvent;
+
+final class CurrentUserSubscriber implements EventSubscriberInterface
+{
+    /**
+     * @var TokenStorageInterface
+     */
+    private $tokenStorage;
+
+    /**
+     * CurrentUserSubscriber constructor.
+     */
+    public function __construct(TokenStorageInterface $tokenStorage)
+    {
+        $this->tokenStorage = $tokenStorage;
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            KernelEvents::VIEW => [['setCurrentUser', EventPriorities::PRE_WRITE]],
+        ];
+    }
+
+    public function setCurrentUser(ViewEvent $event)
+    {
+        $object = $event->getControllerResult();
+
+        $object->setUser($this->tokenStorage->getToken()->getUser());
+        $event->setControllerResult($object);
+    }
+}
