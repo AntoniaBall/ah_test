4feb1ecce6ebd7066588034d006c8981936f8d67
AntoniaBall <antonia.balluais@gmail.com>
2021-02-12 09:47:34 +0100

    jwt sh

--- jwt.sh
+++ jwt.sh
@@ -1,2 +1,2 @@
-"openssl genrsa -out config/jwt/private.pem -aes256 4096",
-"openssl rsa -pubout -in config/jwt/private.pem -out config/jwt/public.pem"
+openssl genrsa -out config/jwt/private.pem -aes256 4096
+openssl rsa -pubout -in config/jwt/private.pem -out config/jwt/public.pem
