f812a34bf1e5acf3d2815b25e7f5485d769c9130
AntoniaBall <antonia.balluais@gmail.com>
2021-03-18 22:08:36 +0100

    filter api properties for user and anonym

--- src/Doctrine/PropertyExtensions.php
+++ src/Doctrine/PropertyExtensions.php
@@ -33,15 +33,18 @@ final class PropertyExtensions implements QueryCollectionExtensionInterface, Que
     
     private function addWhere(QueryBuilder $queryBuilder, string $resourceClass): void
     {
-        if (Property::class !== $resourceClass || $this->security->isGranted('ROLE_ADMIN') || 
-        null === $user = $this->security->getUser()) {
+        if (Property::class !== $resourceClass || $this->security->isGranted('ROLE_ADMIN')) {
             return;
         }
         
-        if ($this->security->isGranted('ROLE_USER')){
+        if ($this->security->isGranted('ROLE_USER') 
+            || null === $user = $this->security->getUser() ){
             $rootAlias = $queryBuilder->getRootAliases()[0];
             $queryBuilder->andWhere(sprintf('%s.isPublished = :isPublished', $rootAlias))
-                        ->setParameter('isPublished', true);
+                        ->setParameter('isPublished', true)
+                        ->andWhere(sprintf('%s.status = :status', $rootAlias))
+                        ->setParameter('status', "acceptee");
+
         }
 
     }
